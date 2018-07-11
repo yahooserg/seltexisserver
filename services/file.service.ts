c067fd29e8265891e2d23ad20a9e695fa6b407dd
yahooserg <seltexinc@gmail.com>
2018-07-11 07:26:44 -0400

    test

--- services/file.service.ts
+++ services/file.service.ts
@@ -54,8 +54,8 @@ export class MyFileService {
   }
 
   public getCertificates() {
-    let privateKey = fs.readFileSync(`${__dirname}/../../../../../etc/letsencrypt/live/seltex.ru/privkey.pem`);
-    let certificate = fs.readFileSync(`${__dirname}/../../../../../etc/letsencrypt/live/seltex.ru/fullchain.pem`);
+    let privateKey = fs.readFileSync(`${__dirname}/../../../../../../etc/letsencrypt/live/seltex.ru/privkey.pem`);
+    let certificate = fs.readFileSync(`${__dirname}/../../../../../../etc/letsencrypt/live/seltex.ru/fullchain.pem`);
     return {key: privateKey, cert: certificate};
   }
 
