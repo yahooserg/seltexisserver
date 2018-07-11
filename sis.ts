274a1e5bf9ad1f8d7c999f73d896785d5d1cf15b
yahooserg <seltexinc@gmail.com>
2018-07-10 23:55:12 -0400

    HTTPS test

--- sis.ts
+++ sis.ts
@@ -35,7 +35,7 @@ app.use(function(req, res, next) {
   }
   // res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "DELETE, PUT");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
   next();
 });
 
