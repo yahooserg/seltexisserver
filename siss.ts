ad345edbe87d4246659737fab3285f82f747a60c
yahooserg <seltexinc@gmail.com>
2018-08-18 19:51:31 +0300

    test

--- siss.ts
+++ siss.ts
@@ -2,16 +2,30 @@
 import * as express from 'express';
 import { Application } from 'express';
 import * as http from 'http';
-const app: Application = express();
-
+import * as fs from 'fs';
+import * as https from 'https';
 import {MyNodeConfig} from '../seltexisserverconfig/mynodeconfig';
 const myNodeConfig = new MyNodeConfig();
+import { MySqlService } from './services/mysql.service';
+const mySqlService = new MySqlService();
+import { MyFileService } from './services/file.service';
+const myFileService = new MyFileService();
+import { MyFunctions } from './services/functions.service';
+const myFunctions = new MyFunctions();
+import { MyXLService } from './services/xls.service';
+const myXLService = new MyXLService();
+import {MyAWSService} from './services/aws.service';
+const myAWSService = new MyAWSService();
+const app: Application = express();
+let bodyParser = require('body-parser');
 
-
-const httpServer = http.createServer(app);
-httpServer.listen(myNodeConfig.serverPort, () => {
-  console.log(myNodeConfig);
- });
+let privateKey = fs.readFileSync(`/etc/letsencrypt/live/seltex.ru/privkey.pem`);
+let certificate = fs.readFileSync(`/etc/letsencrypt/live/seltex.ru/fullchain.pem`);
+let credentials = {key: privateKey, cert: certificate};
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(myNodeConfig.serverPort, () => {
+ console.log(myNodeConfig);
+});
 
 
 // app.get('/api/tempfunc', function(req, res) {
