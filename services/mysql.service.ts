4071998543c8dd1f7f3d7992f86e438b84491ed9
yahooserg <seltexinc@gmail.com>
2018-09-05 16:50:35 +0300

    less data in mysql select

--- services/mysql.service.ts
+++ services/mysql.service.ts
@@ -524,7 +524,7 @@ export class MySqlService {
 
   getPriceListData(company, callback) {
     let items = [];
-    let query = `SELECT * FROM seltexru.inventory where (description like '%cat%' or comment like '%cat%' or description like '%prodiesel%' or comment like '%prodiesel%') and (description not like '%core%' and comment not like '%core%')`;
+    let query = `SELECT i.id, i.description, i.comment, i.price, i.stock, i.ordered, i.msk FROM seltexru.inventory as i where (i.description like '%cat%' or i.comment like '%cat%' or i.description like '%prodiesel%' or i.comment like '%prodiesel%') and (i.description not like '%core%' and i.comment not like '%core%')`;
     let connection = mysql.createConnection(mySqlConnection);
     let request = connection.query(query);
     request
