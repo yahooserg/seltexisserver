f556ab9a97e94e2d8eaf25d315474fc0d2ff4aae
yahooserg <seltexinc@gmail.com>
2018-09-23 11:36:21 -0400

    test with new xl lib

--- services/xls.service.ts
+++ services/xls.service.ts
@@ -25,85 +25,50 @@ export class MyXLService {
     // let worksheet2 = workbook2.addWorksheet('SeltexPrice');
     let priceUpdatedOnInfo: string = `Updated: ${myFunctions.getDateString()}`;
     worksheet.addRow(["id","name","manufacturer", "main number", "all numbers", "price", "stock msk", "stock spb", "transit", priceUpdatedOnInfo]);
+
+
+
+
+
+    for (let i: number = 0; i < data.length; i += 1) {
+      data[i].numbersString = "";
+
+      for(let j: number = 0; j < data[i].numbers.length; j += 1) {
+        if(j === 0) {
+          data[i].manufacturer = data[i].numbers[j].manufacturerFullName;
+          data[i].numberMain = data[i].numbers[j].number;
+          data[i].numbersString += `${data[i].numbers[j].number}`;
+        } else {
+          data[i].numbersString += ` / ${data[i].numbers[j].number}`;
+        }
+      }
+      if (data[i].stock > 12) {
+        data[i].stock = ">12";
+      } else {
+        data[i].stock = `${data[i].stock}`;
+      }
+
+      if (data[i].msk > 12) {
+        data[i].msk = ">12";
+      } else {
+        data[i].msk = `${data[i].msk}`;
+      }
+
+      if (data[i].ordered > 12) {
+        data[i].ordered = ">12";
+      } else {
+        data[i].ordered = `${data[i].ordered}`;
+      }
+
+      worksheet.addRow([data[i].id,`${data[i].description} ${data[i].comment}`,`${data[i].manufacturer}`, `${data[i].numberMain}`, `${data[i].numbersString}`, data[i].price, `${data[i].msk}`, `${data[i].stock}`, `${data[i].ordered}`]);
+
+    }
+
     workbook.xlsx.writeFile("../../../www/seltex/seltexru/data/seltexcross.xlsx")
     .then(function() {
-        // done
+        callback();
     });
-    // let style = workbook.createStyle({
-    //   font: {
-    //     // color: '#FF0800',
-    //     size: 12
-    //   }
-    // });
-    //
-    // // let style2 = workbook2.createStyle({
-    // //   font: {
-    // //     // color: '#FF0800',
-    // //     size: 12
-    // //   }
-    // // });
-    //
-    // // numberFormat: '$#,##0.00; ($#,##0.00); -'
-    // worksheet.cell(1,1).string('id').style(style);
-    // worksheet.cell(1,2).string('name').style(style);
-    // worksheet.cell(1,3).string('manufacturer').style(style);
-    // worksheet.cell(1,4).string('main number').style(style);
-    // worksheet.cell(1,5).string('all numbers').style(style);
-    // worksheet.cell(1,6).string('price').style(style);
-    // worksheet.cell(1,7).string('stock msk').style(style);
-    // worksheet.cell(1,8).string('stock spb').style(style);
-    // worksheet.cell(1,9).string('in transit').style(style);
-    //
-    // // worksheet2.cell(1,1).string('manufacturer').style(style);
-    // // worksheet2.cell(1,2).string('number').style(style);
-    // // worksheet2.cell(1,3).string('crossmanufacturer').style(style);
-    // // worksheet2.cell(1,4).string('crossnumber').style(style);
-    //
-    // let priceUpdatedOnInfo: string = myFunctions.getDateString();
-    // worksheet.cell(1,10).string(`Updated: ${priceUpdatedOnInfo}`).style(style);
-    //
-    //
-    // for (let i: number = 0; i < data.length; i += 1) {
-    //   data[i].numbersString = "";
-    //
-    //   for(let j: number = 0; j < data[i].numbers.length; j += 1) {
-    //     if(j === 0) {
-    //       data[i].manufacturer = data[i].numbers[j].manufacturerFullName;
-    //       data[i].numberMain = data[i].numbers[j].number;
-    //       data[i].numbersString += `${data[i].numbers[j].number}`;
-    //     } else {
-    //       data[i].numbersString += ` / ${data[i].numbers[j].number}`;
-    //     }
-    //   }
-    //   if (data[i].stock > 12) {
-    //     data[i].stock = ">12";
-    //   } else {
-    //     data[i].stock = `${data[i].stock}`;
-    //   }
-    //
-    //   if (data[i].msk > 12) {
-    //     data[i].msk = ">12";
-    //   } else {
-    //     data[i].msk = `${data[i].msk}`;
-    //   }
-    //
-    //   if (data[i].ordered > 12) {
-    //     data[i].ordered = ">12";
-    //   } else {
-    //     data[i].ordered = `${data[i].ordered}`;
-    //   }
-    //
-    //   worksheet.cell(i+2,1).number(data[i].id).style(style);
-    //   worksheet.cell(i+2,2).string(`${data[i].description} ${data[i].comment}`).style(style);
-    //   worksheet.cell(i+2,3).string(`${data[i].manufacturer}`).style(style);
-    //   worksheet.cell(i+2,4).string(`${data[i].numberMain}`).style(style);
-    //   worksheet.cell(i+2,5).string(`${data[i].numbersString}`).style(style);
-    //   worksheet.cell(i+2,6).number(data[i].price).style(style);
-    //   worksheet.cell(i+2,7).string(`${data[i].msk}`).style(style);
-    //   worksheet.cell(i+2,8).string(`${data[i].stock}`).style(style);
-    //   worksheet.cell(i+2,9).string(`${data[i].ordered}`).style(style);
-    //
-    // }
+
     //
     // // workbook.write('../../../www/seltex/seltexru/data/SeltexPrice.xlsx');
     // workbook.write('../../../www/seltex/seltexru/data/seltexprice.xlsx', function(err, stats) {
