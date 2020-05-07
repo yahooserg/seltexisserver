07aa000b64c1d0d4e58faf8c72579ecefd06a433
yahooserg <seltexinc@gmail.com>
2020-05-08 01:01:13 +0700

    test

--- s.ts
+++ s.ts
@@ -1,10 +1,9 @@
 ///<reference path="./node_modules/@types/node/index.d.ts"/>
-// import * as express from 'express';
-// import { Application } from 'express';
+import * as express from 'express';
+import { Application } from 'express';
 // import * as fs from 'fs';
 import {MyNodeConfig} from '../seltexisserverconfig/mynodeconfig';
 const myNodeConfig = new MyNodeConfig();
-console.log(myNodeConfig.serverPort);
 // import { MySqlService } from './services/mysql.service';
 // const mySqlService = new MySqlService();
 // import { MyFileService } from './services/file.service';
@@ -15,19 +14,19 @@ console.log(myNodeConfig.serverPort);
 // const myXLService = new MyXLService();
 // import {MyAWSService} from './services/aws.service';
 // const myAWSService = new MyAWSService();
-// const app: Application = express();
+const app: Application = express();
 // let bodyParser = require('body-parser');
 // // import * as request from 'request';
-// import * as http from 'http';
+import * as http from 'http';
 // import * as https from 'https';
 //
 // //////////////////////////////////////////////////
 // ////////// http/https secure or not block
 // if (!myNodeConfig.secure) {
-//   const server = http.createServer(app);
-//   server.listen(myNodeConfig.serverPort, () => {
-//     console.log("runing http")
-//   });
+  const server = http.createServer(app);
+  server.listen(myNodeConfig.serverPort, () => {
+    console.log("runing http")
+  });
 // } else {
 //   let privateKey = fs.readFileSync(`/etc/letsencrypt/live/seltex.ru/privkey.pem`);
 //   let certificate = fs.readFileSync(`/etc/letsencrypt/live/seltex.ru/fullchain.pem`);
